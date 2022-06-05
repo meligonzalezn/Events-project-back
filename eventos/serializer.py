c5acf72bf1aa7ef03f12702318ba2d996a8b8d2c
perqueza72 <practica22correo@gmail.com>
2022-06-05 16:16:28 -0500

    BD Model added

--- eventos/serializer.py
+++ eventos/serializer.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from .models import Usuario
+from .models import Actividad, Evento, Noticia, Usuario
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = ['Nombre', 'Correo', 'Rol', 'Estado']
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Evento
+        field = ['Titulo', 'Detalles', 'Estado', 'Espacio', 'Media_file', 'Fecha', 'Hora_inicio', 'Hora_finalizacion']
+
+class ActividadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actividad
+        field = ['Fecha', 'Hora_inicio', 'Hora_finalizacion', 'Espacio', 'Estado', 'Detalles', 'Titulo']
+
+class NoticiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Noticia
+        field = ['Titulo', 'Descripcion', 'Resumen', 'Estado', 'Media_file', 'Fecha_edicion']
+
--- eventos/views.py
+++ eventos/views.py
@@ -1,8 +1,22 @@
 from rest_framework import viewsets
-from .serializer import UserSerializer
-from .models import Usuario
+from .serializer import ActividadSerializer, EventSerializer, NoticiaSerializer, UserSerializer
+from .models import Usuario, Evento, Usuario, Actividad, Pago, Noticia
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = Usuario.objects.all()
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    queryset = Evento.objects.all()
+
+class ActividadViewSet(viewsets.ModelViewSet):
+    serializer_class = ActividadSerializer
+    queryset = Actividad.objects.all()
+
+class NoticiaViewSet(viewsets.ModelViewSet):
+    serializer_class = NoticiaSerializer
+    queryset = Noticia.objects.all()
+    
+

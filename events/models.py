c5d6b9b73e49247014e4045c0b326e753db96178
meliwi <melissa.gonzalez@correounivalle.edu.co>
2022-06-18 23:22:43 -0500

    change event imageField

--- events/models.py
+++ events/models.py
@@ -1,10 +1,5 @@
 from django.db import models    
-# from activities.models import Activity
 from users.models import User
-
-import cloudinary
-from cloudinary.models import CloudinaryField
-# Create your models here.
 
 
 class Event(models.Model):
@@ -13,14 +8,9 @@ class Event(models.Model):
     State = models.CharField(max_length=20)
     Space = models.CharField(max_length=20)
     Cost = models.IntegerField(default=0)
-    ##images_events/
-    Media_file = cloudinary.models.CloudinaryField(folder='media/images_videos_news/', overwrite=True, resource_type='')
+    Media_file = models.ImageField(upload_to='images_events/')
     Start_date = models.DateField()
     Finish_date = models.DateField()
-
-
-
-
 
 class Payment(models.Model):
     ID_User = models.ForeignKey(User, on_delete=models.CASCADE)

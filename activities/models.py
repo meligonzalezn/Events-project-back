9d5d4f12fd6042fdfef30bbb2f8902612d683454
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-07-22 19:17:24 -0500

    Capacity field added to activity

--- activities/models.py
+++ activities/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from events.models import Event
 
 # Create your models here.
+
+
 class Activity(models.Model):
     Date = models.DateField()
     Init_hour = models.TimeField()
@@ -10,4 +12,5 @@ class Activity(models.Model):
     State = models.CharField(max_length=51)
     Details = models.CharField(max_length=100)
     Title = models.CharField(max_length=100)
+    Capacity = models.IntegerField(default=100)
     ID_Event = models.ForeignKey(Event, on_delete=models.CASCADE)

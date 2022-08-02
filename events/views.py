6ed5706e8206296fef64c6a4a886ab389036a1c9
michelle-gh <michelle.hernandez@correounivalle.edu.co>
2022-08-02 18:32:30 -0500

    update event  added

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from urllib.request import Request
 from rest_framework import viewsets
 from .serializer import EventSerializer
-from .models import User, Event
+from .models import Event
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,62 +10,41 @@ from rest_framework import status
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    model = Event
     serializer_class = EventSerializer
     queryset = Event.objects.all()
 
-    @action(detail=True, methods=['get'], url_path='bring')
-    def traer(self, request, format=None):
-        
-        serializer = EventSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response("monda", status=status.HTTP_400_BAD_REQUEST)
-    
-    @action(detail=True, methods=['put'], url_path='update')
-    def actualizar(this, request, pk):
-        # try:
+
+    @action(detail=True, methods=['put'])
+    def update_event(this, request, pk):
+        try:
             event = Event.objects.get(id=pk)
-            
             if('Title' in request.data):
                 event.Title = request.data['Title']
-
+            if('Details' in request.data):
+                event.Description = request.data['Details']
+            if('Space' in request.data):
+                event.Summary = request.data['Space']
+            if('State' in request.data):
+                event.State = request.data['State']
+            if('Cost' in request.data):
+                event.State = request.data['Cost']
+            if('Media_file' in request.data):
+                event.Media_file = request.data['Media_file']
+            if('Start_date' in request.data):
+                event.Edition_date = request.data['Start_date']
+            if('Finish_date' in request.data):
+                event.Edition_date = request.data['Finish_date']
             event.save()
 
-            return Response("User " + event.Title + " updated", status=status.HTTP_200_OK)
+            return Response("Event " + event.Title + " updated", status=status.HTTP_200_OK)
+        except:
+            return Response("Error", status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
 
 
-    @action(detail=True, methods=['post'], url_path='crea')
-    def crea(this, request, pk):
-        # try:
-            user = User.objects.get(id=pk)
-            
-            if('Name' in request.data):
-                user.Name = request.data['Name']
-            if('State' in request.data):
-                user.State = request.data['State']
-            if('Role' in request.data):
-                user.Role = request.data['Role'] 
-            if('Email' in request.data):
-                user.Email = request.data['Email']
-            if('Phone' in request.data):
-                user.Phone = request.data['Phone']
-            if('Password' in request.data):
-                user.Password = request.data['Password']
-            user.save()
 
-            return Response("User " + user.Name + " updated", status=status.HTTP_200_OK)
-    # @action(detail=True, methods=['get'], url_path='watch')
-    # ## 
-    # def get_events(request: Request):
-    #     try:
-    #         query = Event.objects.all()
-    #         serializer: EventSerializer = self.serializer_class(query, many=False)
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     except:
-    #         return Response("Events don't exist", status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 

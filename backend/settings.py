0c001df40ce977205aad97fdff0e14b03345f847
Dhliv <david.henao.martinez@correounivalle.edu.co>
2022-08-04 19:28:28 -0500

    testing backend

--- backend/settings.py
+++ backend/settings.py
@@ -54,9 +54,8 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
